966a800bcb086a0bbcde6551ac07807f1b972a98
Joel Myrtil <myrtil.joel@gmail.com>
2023-03-09 05:08:25 -0800

    edit

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
+import Carousel from 'react-bootstrap';
 
 class Movie extends React.Component {
 
   render() {
-    
+
+    console.log(this.props.movieInfo);
+
     return (
-      <>
-      onChange={this.handleMovies}
-      </>
+      
+
+      this.props.movieInfo.map((movie, idx) => (
+        <>
+            <Carousel>
+              < Carousel.Item key={idx}>
+                <img
+                  className="carpic"
+                  src={movie.imageurl}
+                  alt={''.alt}
+                />
+              </Carousel.Item >
+            </Carousel>
+        </>
+      ))
     );
   }
 }
+
 
 export default Movie;
